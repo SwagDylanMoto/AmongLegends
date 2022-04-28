e2b5ffad42d478c549479a26b8ac3a811c4ebb87
Dabmuel <samuel.jung@viacesi.fr>
2022-04-28 14:48:15 +0200

    endstatdto

--- code/dto/EndStatDTO.php
+++ code/dto/EndStatDTO.php
@@ -3,4 +3,12 @@
 class EndStatDTO extends DTO {
 
     public $gameId;
+
+    public $win;
+
+    public $mostDmg_GameSessionId;
+
+    public $mostDeath_GameSessionId;
+
+    public $mostKill_GameSessionId;
 }
